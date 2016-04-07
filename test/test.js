dda286b7f62eaa58488249be011a0a607688a30b
Waratnan Suriyasorn <waratnansuriyasorn@Wasins-Mac-mini.local>
2016-04-07 13:14:41 +0700

    remove test slow

--- test/test.js
+++ test/test.js
@@ -82,11 +82,3 @@ describe('add()', function() {
     });
   });
 });
-
-describe('something slow', function() {
-  this.slow(10000);
-
-  it('should take long enough for me to go make a sandwich', function() {
-    // ...
-  });
-});
